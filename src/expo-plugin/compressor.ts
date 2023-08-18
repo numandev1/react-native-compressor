96d54523bd71697c05414624c3ebcbda3b25b6a1
Muhammad Numan <muhammadnuman70@gmail.com>
2023-08-19 04:40:51 +0500

    chore: update react native to 0.72.4

--- src/expo-plugin/compressor.ts
+++ src/expo-plugin/compressor.ts
@@ -1,8 +1,9 @@
+//@ts-ignore
 import { ConfigPlugin, createRunOncePlugin } from '@expo/config-plugins';
 const pkg = require('../../../package.json');
 
 type Props = {};
 
-const withCompressor: ConfigPlugin<Props> = (config) => config;
+const withCompressor: ConfigPlugin<Props> = (config:any) => config;
 
 export default createRunOncePlugin(withCompressor, pkg.name, pkg.version);

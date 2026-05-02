81f2ed31fbf0d1fd35f1ee1c05cd882e7f4c9ba5
Numan <muhammadnuman70@gmail.com>
2026-05-02 05:28:46 +0500

    chore: add test cases

---
+++ examples/bare/harness/native-compressor.harness.ts
@@ -0,0 +1,1 @@
+import '../../../harness/native-compressor.harness';
--- examples/bare/index.js
+++ examples/bare/index.js
@@ -2,6 +2,7 @@
  * @format
  */
 
+import './harnessCallableModules';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
